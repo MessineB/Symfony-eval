6d2b515bcf537a76040b20f63125b0672ffcc658
MessineB <messinebelarousi@gmail.com>
2022-04-04 10:48:24 +0200

    first commit

---
+++ src/Controller/ProfileController.php
@@ -0,0 +1,21 @@
+<?php
+
+namespace App\Controller;
+
+use App\Repository\PostRepository;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Annotation\Route;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+
+class ProfileController extends AbstractController
+{
+    #[Route('/profile', name: 'app_profile')]
+    public function index(PostRepository $postrepo,): Response
+    { 
+        $user = $this->getUser();
+        $posts = $postrepo->findByUserid($user);
+        return $this->render('profile/index.html.twig', [
+            'posts' => $posts,
+        ]);
+    }
+}
--- src/Repository/PostRepository.php
+++ src/Repository/PostRepository.php
@@ -64,19 +64,18 @@ class PostRepository extends ServiceEntityRepository
     // /**
     //  * @return Post[] Returns an array of Post objects
     //  */
-    /*
-    public function findByExampleField($value)
+    
+    public function findByUserid($value)
     {
         return $this->createQueryBuilder('p')
-            ->andWhere('p.exampleField = :val')
+            ->andWhere("p.user = :val")
             ->setParameter('val', $value)
-            ->orderBy('p.id', 'ASC')
-            ->setMaxResults(10)
+            ->orderBy('p.createdAt', 'ASC')
             ->getQuery()
             ->getResult()
         ;
     }
-    */
+    
 
     /*
     public function findOneBySomeField($value): ?Post
