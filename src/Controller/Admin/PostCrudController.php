7ba9d25abaaabe16769832c685816bb35bd8de7f
AurelienGEORGES <aurelien.georges30@gmail.com>
2022-04-05 16:12:48 +0200

    avant recuperation du travail final

--- src/Controller/Admin/PostCrudController.php
+++ src/Controller/Admin/PostCrudController.php
@@ -4,11 +4,12 @@ namespace App\Controller\Admin;
 
 use App\Entity\Post;
 use EasyCorp\Bundle\EasyAdminBundle\Field\IdField;
-use EasyCorp\Bundle\EasyAdminBundle\Field\TextField;
-use EasyCorp\Bundle\EasyAdminBundle\Controller\AbstractCrudController;
-use EasyCorp\Bundle\EasyAdminBundle\Field\ArrayField;
 use EasyCorp\Bundle\EasyAdminBundle\Field\DateField;
+use EasyCorp\Bundle\EasyAdminBundle\Field\TextField;
+use EasyCorp\Bundle\EasyAdminBundle\Field\ArrayField;
 use EasyCorp\Bundle\EasyAdminBundle\Field\ImageField;
+use EasyCorp\Bundle\EasyAdminBundle\Field\AssociationField;
+use EasyCorp\Bundle\EasyAdminBundle\Controller\AbstractCrudController;
 
 class PostCrudController extends AbstractCrudController
 {
@@ -25,7 +26,8 @@ class PostCrudController extends AbstractCrudController
             TextField::new('content'),
             ArrayField::new('status'),
             ImageField::new('picture')->setUploadDir('/public/img/post'),
-            DateField::new('created_at')
+            DateField::new('created_at'),
+            AssociationField::new('user')->hideOnForm(),
         ];
     }
    
