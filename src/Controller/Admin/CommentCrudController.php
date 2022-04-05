b8cfa8c056dd5d157453ef3cae7ed321d10ddf8c
AidounNejma <guermachenejma@gmail.com>
2022-04-05 11:02:11 +0200

    Update CommentCrudController.php

--- src/Controller/Admin/CommentCrudController.php
+++ src/Controller/Admin/CommentCrudController.php
@@ -6,9 +6,10 @@ use App\Entity\Comment;
 use EasyCorp\Bundle\EasyAdminBundle\Field\IdField;
 use EasyCorp\Bundle\EasyAdminBundle\Field\TextField;
 use EasyCorp\Bundle\EasyAdminBundle\Field\ArrayField;
-use EasyCorp\Bundle\EasyAdminBundle\Field\DateTimeField;
-use EasyCorp\Bundle\EasyAdminBundle\Controller\AbstractCrudController;
 use EasyCorp\Bundle\EasyAdminBundle\Field\IntegerField;
+use EasyCorp\Bundle\EasyAdminBundle\Field\DateTimeField;
+use EasyCorp\Bundle\EasyAdminBundle\Field\AssociationField;
+use EasyCorp\Bundle\EasyAdminBundle\Controller\AbstractCrudController;
 
 class CommentCrudController extends AbstractCrudController
 {
@@ -22,6 +23,8 @@ class CommentCrudController extends AbstractCrudController
     {
         return [
             IdField::new('id'),
+            AssociationField::new('post'),
+            AssociationField::new('user'),
             TextField::new('content'),
             ArrayField::new('status'),
             DateTimeField::new('created_at'),
